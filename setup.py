378416074c1860f90df25afe72dcea5a451392b3
AsmSafone <77989182+AsmSafone@users.noreply.github.com>
2023-01-10 19:45:59 +0600

    v1.0.30
    - python3.11 support added

--- setup.py
+++ setup.py
@@ -29,10 +29,12 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
+        "Natural Language :: English",
     ],
-    python_requires=">3.6, <3.11",
+    python_requires="~=3.7",
 )
